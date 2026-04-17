5529b1c0e2293471e792d2d03fcb27f01fdb4356
fei <liufei787@gmail.com>
2026-04-17 19:13:57 +0200

    implement fbx mesh importer

--- FbxMeshImporter.cpp
+++ FbxMeshImporter.cpp
@@ -6,20 +6,34 @@
 //-----------------------------------------------------------------
 // Include Files
 //-----------------------------------------------------------------
+#include <memory>
 #include "FbxMeshImporter.h"
 #include "StringUtilities.h"
 #include "Mesh.h"
+#include "Vector2.h"
+#include "Vector3.h"
 
 using namespace Emerald;
 
 FbxMeshImporter::FbxMeshImporter()
 {
-
+    serial = 1;
+    ClearAnomaly();
 }
 
 FbxMeshImporter::~FbxMeshImporter()
 {
 
+}
+
+size_t FbxMeshImporter::GetNextSerial()
+{
+    return serial++;
+}
+
+void FbxMeshImporter::ClearAnomaly()
+{
+    anomalyFlags = '\0';
 }
 
 bool FbxMeshImporter::LoadAssetFromFile(LPCWSTR pFileName)
@@ -61,20 +75,203 @@ void FbxMeshImporter::ParseScene()
 
 void FbxMeshImporter::ExtractMesh(FbxNode* pNode)
 {
+    Error localError;
     FbxNodeAttribute* pNodeAttribute = pNode->GetNodeAttribute();
-    if (pNodeAttribute)
+    if (pNodeAttribute && pNodeAttribute->GetAttributeType() == FbxNodeAttribute::eMesh)
     {
-        if (pNodeAttribute->GetAttributeType() == FbxNodeAttribute::eMesh)
+        size_t id = GetNextSerial();
+        Mesh* pMyMesh = new Mesh(id, std::to_wstring(id));
+        FbxMesh* pFbxMesh = pNode->GetMesh();
+        Vector3* pPositions = nullptr;
+        Vector3* pNormals = nullptr;
+        Vector3* pBinormals = nullptr;
+        Vector3* pTangents = nullptr;
+        Vector2* pUVs = nullptr;
+
+        const int triangleCount = pFbxMesh->GetPolygonCount();
+        const int controlPointCount = pFbxMesh->GetControlPointsCount();
+        if (triangleCount * 3 == controlPointCount)
         {
-            FbxMesh* pMesh = pNode->GetMesh();
-            int triangleCount = pMesh->GetPolygonCount();
-            int controlPointCount = pMesh->GetControlPointsCount();
-            if (triangleCount * 3 != controlPointCount)
-            {
-                MessageBox(NULL, TEXT("Triangle vertices are not equal to control points!"), TEXT("ERROR"), MB_OK | MB_ICONEXCLAMATION);
-            }
-            FbxVector4* controlPoints = pMesh->GetControlPoints();
+            anomalyFlags |= NON_INDEXEDPOSITION;
+            goto ANOMALYCLEAR;
         }
+
+        //extract positions
+        FbxVector4* controlPoints = pFbxMesh->GetControlPoints();
+        pPositions = new Vector3[controlPointCount];
+        for (int i = 0; i < controlPointCount; i++)
+        {
+            pPositions[i].x = controlPoints[i][0];
+            pPositions[i].y = controlPoints[i][1];
+            pPositions[i].z = controlPoints[i][2];
+        }
+        pMyMesh->SetPositions(pPositions, controlPointCount, localError);
+        if (localError)
+        {
+            goto ANOMALYCLEAR;
+        }
+
+        //extract normals
+        FbxLayer* pLayer = pFbxMesh->GetLayer(0);
+        if (!pLayer)
+        {
+            anomalyFlags |= NO_LAYER;
+            goto ANOMALYCLEAR;
+        }
+        FbxLayerElementNormal* pFbxNormals = pLayer->GetNormals();
+        if (!pFbxNormals || pFbxNormals->GetMappingMode() != FbxLayerElement::eByControlPoint)
+        {
+            anomalyFlags |= ILL_NORMAL;
+            goto ANOMALYCLEAR;
+        }
+        bool isIndirect = pFbxNormals->GetReferenceMode() == FbxLayerElement::eIndexToDirect;
+        pNormals = new Vector3[controlPointCount];
+        for (int i = 0; i < controlPointCount; i++)
+        {
+            int index = i;
+            if (isIndirect)
+            {
+                //i is index to the layer element's index array instead
+                index = pFbxNormals->GetIndexArray().GetAt(index);
+            }
+            pNormals[i].x = (pFbxNormals->GetDirectArray().GetAt(index))[0];
+            pNormals[i].y = (pFbxNormals->GetDirectArray().GetAt(index))[1];
+            pNormals[i].z = (pFbxNormals->GetDirectArray().GetAt(index))[2];
+        }
+        pMyMesh->SetNormals(pNormals, controlPointCount, localError);
+        if (localError)
+        {
+            goto ANOMALYCLEAR;
+        }
+
+        //extract uvs, optional
+        FbxLayerElementUV* pFbxUVs = pLayer->GetUVs();
+        if (pFbxUVs)
+        {
+            if (pFbxUVs->GetMappingMode() != FbxLayerElement::eByControlPoint)
+            {
+                anomalyFlags |= ILL_UV;
+                goto ANOMALYCLEAR;
+            }
+            isIndirect = pFbxUVs->GetReferenceMode() == FbxLayerElement::eIndexToDirect;
+            pUVs = new Vector2[controlPointCount];
+            for (int i = 0; i < controlPointCount; i++)
+            {
+                int index = i;
+                if (isIndirect)
+                {
+                    //i is index to the layer element's index array instead
+                    index = pFbxUVs->GetIndexArray().GetAt(index);
+                }
+                pUVs[i].x = (pFbxUVs->GetDirectArray().GetAt(index))[0];
+                pUVs[i].y = (pFbxUVs->GetDirectArray().GetAt(index))[1];
+            }
+            pMyMesh->SetUVs(pUVs, controlPointCount, localError);
+            if (localError)
+            {
+                goto ANOMALYCLEAR;
+            }
+        }
+
+        //extract binormals, optional
+        FbxLayerElementBinormal* pFbxBinormals = pLayer->GetBinormals();
+        if (pFbxBinormals)
+        {
+            if (pFbxBinormals->GetMappingMode() != FbxLayerElement::eByControlPoint)
+            {
+                anomalyFlags |= ILL_BINORMAL;
+                goto ANOMALYCLEAR;
+            }
+            isIndirect = pFbxBinormals->GetReferenceMode() == FbxLayerElement::eIndexToDirect;
+            pBinormals = new Vector3[controlPointCount];
+            for (int i = 0; i < controlPointCount; i++)
+            {
+                int index = i;
+                if (isIndirect)
+                {
+                    //i is index to the layer element's index array instead
+                    index = pFbxBinormals->GetIndexArray().GetAt(index);
+                }
+                pBinormals[i].x = (pFbxBinormals->GetDirectArray().GetAt(index))[0];
+                pBinormals[i].y = (pFbxBinormals->GetDirectArray().GetAt(index))[1];
+                pBinormals[i].z = (pFbxBinormals->GetDirectArray().GetAt(index))[2];
+            }
+            pMyMesh->SetBinormals(pBinormals, controlPointCount, localError);
+            if (localError)
+            {
+                goto ANOMALYCLEAR;
+            }
+        }
+
+        //extract tangent, optional
+        FbxLayerElementTangent* pFbxTangents = pLayer->GetTangents();
+        if (pFbxTangents)
+        {
+            if (pFbxTangents->GetMappingMode() != FbxLayerElement::eByControlPoint)
+            {
+                anomalyFlags |= ILL_TANGENT;
+                goto ANOMALYCLEAR;
+            }
+            isIndirect = pFbxTangents->GetReferenceMode() == FbxLayerElement::eIndexToDirect;
+            pTangents = new Vector3[controlPointCount];
+            for (int i = 0; i < controlPointCount; i++)
+            {
+                int index = i;
+                if (isIndirect)
+                {
+                    //i is index to the layer element's index array instead
+                    index = pFbxTangents->GetIndexArray().GetAt(index);
+                }
+                pTangents[i].x = (pFbxTangents->GetDirectArray().GetAt(index))[0];
+                pTangents[i].y = (pFbxTangents->GetDirectArray().GetAt(index))[1];
+                pTangents[i].z = (pFbxTangents->GetDirectArray().GetAt(index))[2];
+            }
+            pMyMesh->SetTangents(pTangents, controlPointCount, localError);
+            if (localError)
+            {
+                goto ANOMALYCLEAR;
+            }
+        }
+
+        extractedMeshes.push_back(pMyMesh);
+
+        ANOMALYCLEAR:
+        std::wstring errorText(localError.GetErrorText());
+        if (anomalyFlags & NON_INDEXEDPOSITION)
+        {
+            errorText += L"Mesh vertices are not indexed.\n";
+        }
+        if (anomalyFlags & NO_LAYER)
+        {
+            errorText += L"Mesh has no layer.\n";
+        }
+        if (anomalyFlags & ILL_NORMAL)
+        {
+            errorText += L"Mesh normals are ill formed.\n";
+        }
+        if (anomalyFlags & ILL_UV)
+        {
+            errorText += L"Mesh uvs are ill formed.\n";
+        }
+        if (anomalyFlags & ILL_BINORMAL)
+        {
+            errorText += L"Mesh binormals are ill formed.\n";
+        }
+        if (anomalyFlags & ILL_TANGENT)
+        {
+            errorText += L"Mesh tangents are ill formed.\n";
+        }
+        if (errorText.length() != 0)
+        {
+            MessageBox(NULL, errorText.c_str(), TEXT("ERROR"), MB_OK | MB_ICONEXCLAMATION);
+        }
+        SAFE_DELETEARRAY(pPositions);
+        SAFE_DELETEARRAY(pNormals);
+        SAFE_DELETEARRAY(pBinormals);
+        SAFE_DELETEARRAY(pTangents);
+        SAFE_DELETEARRAY(pUVs);
+        SAFE_DELETE(pMyMesh);
+        ClearAnomaly();
     }
 
     const int childNodeCount = pNode->GetChildCount();
--- FbxMeshImporter.h
+++ FbxMeshImporter.h
@@ -12,13 +12,20 @@
 #include "FbxSdkCommon.h"
 #include "StringUtilities.h"
 
+#define NO_LAYER 0x1
+#define NON_INDEXEDPOSITION 0x2
+#define ILL_NORMAL 0x4
+#define ILL_UV 0x8
+#define ILL_BINORMAL 0x10
+#define ILL_TANGENT 0x20
+
 namespace Emerald
 {
     class Mesh;
     class FbxMeshImporter : public IAssetImporter
     {
     public:
-        std::vector<Mesh*> pExtractedMeshes;
+        std::vector<Mesh*> extractedMeshes;//TODO: don't delete the stored pointers yet. they are managed by MeshManager for now.
 
         bool LoadAssetFromFile(LPCWSTR pFileName) override;
         void ParseScene();
@@ -26,8 +33,29 @@ namespace Emerald
         FbxMeshImporter();
         virtual ~FbxMeshImporter();
     private:
+        /*32 bits:
+        * 31...7 6 5 4 3 2 1 0
+        * 
+        * 31: reserved
+        * .
+        * .
+        * .
+        * 7: reserved
+        * 6: reserved
+        * 5: ILL_TANGENT
+        * 4: ILL_BINORMAL
+        * 3: ILL_UV
+        * 2: ILL_NORMAL
+        * 1: NON_INDEXEDPOSITION
+        * 0: NO_LAYER
+        */
+        unsigned int anomalyFlags;
+
         FbxSdkCommon fbxSdk;
+        size_t serial;
 
         void ExtractMesh(FbxNode* pNode);
+        size_t GetNextSerial();
+        void ClearAnomaly();
     };
 }
